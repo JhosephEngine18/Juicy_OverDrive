84c73ff8711983b607a70b9bfc5a1e70931abb4b
GameDevJ180 <josesielo1805@gmail.com>
2025-09-19 10:22:57 -0600

    Update Check_Checkpoints.cs

--- Assets/Scripts/Check_Checkpoints.cs
+++ Assets/Scripts/Check_Checkpoints.cs
@@ -2,15 +2,64 @@ using UnityEngine;
 
 public class Check_Checkpoints : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    int CheckpointsPassed;
+    private void OnTriggerEnter(Collider other)
     {
-        
+        Checkpoints(other);
     }
 
-    // Update is called once per frame
-    void Update()
+    void Checkpoints(Collider other)
     {
-        
+        if (other.gameObject.CompareTag("CheckPoint_0"))
+        {
+            other.gameObject.SetActive(false);
+            CheckpointsPassed++;
+            print(CheckpointsPassed);
+        }
+
+        if (other.gameObject.CompareTag("CheckPoint_1") && CheckpointsPassed == 1)
+        {
+            other.gameObject.SetActive(false);
+            CheckpointsPassed++;
+            print(CheckpointsPassed);
+        }
+        else if (other.gameObject.CompareTag("CheckPoint_1") && CheckpointsPassed != 1)
+        {
+            print("WrongCheckpoint");
+        }
+
+        if (other.gameObject.CompareTag("CheckPoint_2") && CheckpointsPassed == 2)
+        {
+            other.gameObject.SetActive(false);
+            CheckpointsPassed++;
+            print(CheckpointsPassed);
+        }
+        else if (other.gameObject.CompareTag("CheckPoint_2") && CheckpointsPassed != 2)
+        {
+            print("WrongCheckpoint");
+        }
+
+        if (other.gameObject.CompareTag("CheckPoint_3") && CheckpointsPassed == 3)
+        {
+            other.gameObject.SetActive(false);
+            CheckpointsPassed++;
+            print(CheckpointsPassed);
+        }
+        else if (other.gameObject.CompareTag("CheckPoint_3") && CheckpointsPassed != 3)
+        {
+            print("WrongCheckpoint");
+        }
+
+        if (other.gameObject.CompareTag("CheckPoint_3") && CheckpointsPassed == 4)
+        {
+            other.gameObject.SetActive(false);
+            CheckpointsPassed++;
+            print(CheckpointsPassed);
+        }
+        else if (other.gameObject.CompareTag("CheckPoint_3") && CheckpointsPassed != 4)
+        {
+            print("WrongCheckpoint");
+        }
+
     }
 }
