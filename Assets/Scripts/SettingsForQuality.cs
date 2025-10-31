11c296bab1ecf89426b8c19b123dff865200be9b
GameDevJ180 <josesielo1805@gmail.com>
2025-10-31 10:58:35 -0600

    PauseMenu

--- Assets/Scripts/SettingsForQuality.cs
+++ Assets/Scripts/SettingsForQuality.cs
@@ -1,24 +1,43 @@
+using System;
+using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class SettingsForQuality : MonoBehaviour
 {
-    public Toggle fullscreen, vsync;
     public TMP_Dropdown Resolutions;
-    void Start()
-    {
-        fullscreen.isOn = Screen.fullScreen;
+    bool isFullScreen = true;
+    public Toggle ScreenMode;
+    [SerializeField] Scene currentScene;
 
-        if (QualitySettings.vSyncCount == 0)
-        {
-            vsync.isOn = false;
+    //Changes the ScreenMode if is FullScreen or not
+
+    private void Awake()
+    {
+        OnValueChanged(PlayerPrefs.GetInt("Resolutions", 0));
+        isFullScreen = PlayerPrefs.GetInt("ScreenMode", 1) != 0;
+        OnScreenModeChanged(isFullScreen);
+    }
+    public void OnScreenModeChanged(bool isFullScreened)
+    {
+
+        if (isFullScreened)
+        { 
+            Screen.fullScreen = true;
+            ScreenMode.isOn = true;
         }
-        else
+        else if (!isFullScreened)
         {
-            vsync.isOn = true;
+            Screen.fullScreen = false;
+            ScreenMode.isOn = false;
         }
+
+        isFullScreen = isFullScreened;
+        PlayerPrefs.SetInt("ScreenMode", isFullScreened ? 1 : 0);
+        PlayerPrefs.Save();
     }
 
 
@@ -27,20 +46,26 @@ public class SettingsForQuality : MonoBehaviour
         switch (sel)
         {
             case 0:
-                Screen.SetResolution(1920, 1080, fullscreen);
+                Screen.SetResolution(1920, 1080, isFullScreen);
                 print("1920x1080");
+                Resolutions.value = 0;
                 break;
 
             case 1:
-                Screen.SetResolution(1280,720,fullscreen);
+                Screen.SetResolution(1280, 720, isFullScreen);
                 print("1280x720");
+                Resolutions.value = 1;
                 break;
 
             case 2:
-                Screen.SetResolution(640, 480, fullscreen);
-                print("640x480");
+                Screen.SetResolution(640, 360, isFullScreen);
+                print("640x360");
+                Resolutions.value = 2;
                 break;
         }
+
+        PlayerPrefs.SetInt("Resolutions", sel);
+        PlayerPrefs.Save();
     }
 
 }
