b3a2a91c37576f04282d3fdc590e4c75b4385f55
GameDevJ180 <josesielo1805@gmail.com>
2025-12-02 20:05:35 -0600

    newscripts

--- Assets/Scripts/AudioSystem.cs
+++ Assets/Scripts/AudioSystem.cs
@@ -1,19 +1,30 @@
+using System;
+using FMOD.Studio;
+using FMODUnity;
 using UnityEngine;
 
 public class AudioSystem : MonoBehaviour
 {
     private Rigidbody rb;
+    [Header("Car Sound Effects")]
+    public EventReference PitchCarSound;
+    private EventInstance  PitchCarInstance;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        PitchCarInstance = RuntimeManager.CreateInstance(PitchCarSound);
+        PitchCarInstance.start();
+        PitchCarInstance.setVolume(0.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (rb.linearVelocity == Vector3.zero)
-        {
-            
-        }
+        PitchCarInstance.setPitch(Math.Clamp(rb.linearVelocity.magnitude * 0.05f, 0, 1));
+    }
+
+    private void OnDestroy()
+    {
+        PitchCarInstance.release();
     }
 }
--- Assets/Scripts/CrashScript.cs
+++ Assets/Scripts/CrashScript.cs
@@ -1,13 +1,20 @@
 using System;
 using System.Collections;
+using FMOD;
+using FMOD.Studio;
+using FMODUnity;
 using Unity.Cinemachine;
 using UnityEngine;
+using STOP_MODE = FMOD.Studio.STOP_MODE;
 
 public class CrashScript : MonoBehaviour
 {
+    public EventReference Crash;
+        private EventInstance Crashed;
     private void Awake()
     {
         shake.enabled = false;
+        Crashed.setVolume(50);
     }
 
     public CinemachineBasicMultiChannelPerlin shake;
@@ -16,6 +23,7 @@ public class CrashScript : MonoBehaviour
         if (other.gameObject.CompareTag("npc") || other.gameObject.CompareTag("Walls"))
         {
             shake.enabled = true;
+            RuntimeManager.PlayOneShotAttached(Crash, gameObject);
             StartCoroutine(StopShake());
         }
     }
